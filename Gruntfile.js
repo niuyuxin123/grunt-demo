3bebe809fb35c4c1e6180ec54efdc9cdc6c22716
niuyuxin <1120733074@qq.com>
2020-03-06 17:29:37 +0800

    imagemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,18 @@
 
 module.exports = function (grunt) {
  grunt.initConfig({
-    uglify: {
-      release:{
-        files: {
-          'rectangle.min.js': 'rectangle.js'
-        }
-      }       
+    imagemin: {                               
+      files: {
+        expand: true,
+        src: ['images/*.{png,jpg,gif}'],
+        dest: 'dist/'
+      }
     }
   });
 
-  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
 
-  grunt.registerTask('default', ['uglify']); 
+  grunt.registerTask('default', ['imagemin']);     
 
 };
     
