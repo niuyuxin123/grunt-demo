1e40878d4932a6893c49562b62980a98074bd3da
niuyuxin <1120733074@qq.com>
2020-03-10 14:02:54 +0800

    add

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,18 +1,9 @@
 
-module.exports = function (grunt) {
- grunt.initConfig({
-    sprite: {
-      files: {
-        src: 'images/*.png',
-        dest: 'dist/bundle.png',
-        destCss: 'dist/sprite.css'
-      }
-    }
+module.exports = function (grunt) { 
+
+  grunt.registerTask('build','build task',function(){
+    console.log('build task!')
   });
-
-  grunt.loadNpmTasks('grunt-spritesmith');
-
-  grunt.registerTask('default', ['sprite']);
       
 
 };
